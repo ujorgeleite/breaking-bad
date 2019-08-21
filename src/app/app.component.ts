26e766eede8bf2d0484d239bac7b136cf1a10a27
Jorge Leite <jorgefernandodamasio@gmail.com>
2019-08-21 11:36:27 -0300

    feat(app-component): add another components to principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotificationModel } from './notification-modal/model/notification.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'breaking-bad';
+  modalMessage = '';
+  modalTitle = '';
+
+
+  receiverEvent(event: NotificationModel) {
+    this.modalTitle = event.title;
+    this.modalMessage = event.message;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { ApiService} from './commons/services/api.service';
+import { CharacterListModule } from './character-list/character-list.module';
+import { NotificationModalComponent } from './notification-modal/notification-modal.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NotificationModalComponent],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    HttpClientModule,
+    CharacterListModule,
+    NgbModalModule],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
