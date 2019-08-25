3c0ab7bd89b748fd00309b9b55c429a0cd39ceba
Jorge Leite <jorgefernandodamasio@gmail.com>
2019-08-25 19:05:03 -0300

    refactor(character-list): refactor app module, character list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,25 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { ApiService} from './commons/services/api.service';
-import { CharacterListModule } from './character-list/character-list.module';
-import { NotificationModalComponent } from './notification-modal/notification-modal.component';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { ApiService } from './commons/services/api.service';
 import { PageButtonsService } from './commons/services/page-buttons.service';
+import { CharacterListModule } from './character-list/character-list.module';
+import { NotificationModalModule } from './notification-modal/notification-modal.module';
 
 
 @NgModule({
-  declarations: [AppComponent, NotificationModalComponent],
+  declarations: [AppComponent],
   imports: [
     CommonModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgbModalModule,
     CharacterListModule,
-    NgbModalModule],
+    NotificationModalModule],
   providers: [ApiService, PageButtonsService],
   bootstrap: [AppComponent]
 })
