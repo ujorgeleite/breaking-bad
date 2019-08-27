d44a2252c1dcee05dc020052f9ac6aa295c12e31
Jorge Leite <jorgefernandodamasio@gmail.com>
2019-08-27 09:34:25 -0300

    refactor(test): remove unused test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,11 +9,6 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Select your preferred character in breaking-bad!');
-  });
-
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
